c92f45daa4ec6bee000bc7c300c5d8643a42dada
rnori-harv <rnori@college.harvard.edu>
2023-07-21 17:57:03 -0700

    updating backend

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mindwell-backend.wsgi
--- mern/backend/server/settings.py
+++ mern/backend/server/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,3 +140,4 @@ CORS_ALLOW_METHODS = [
 ]
 
 ALLOWED_HOSTS = ['mindwell-backend-d09bc890953b.herokuapp.com']
+django_heroku.settings(locals())
